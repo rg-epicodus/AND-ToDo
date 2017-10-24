2ba498eea08ecc79cfd7b86f9fe59b65a169d45f
Rich Garrick <richg341@gmail.com>
2017-10-23 17:29:06 -0700

    initialize project

---
+++ app/src/main/java/com/epicodus/todo/RecyclerViewAdapter.java
@@ -0,0 +1,37 @@
+package com.epicodus.todo;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by OIG on 10/23/2017.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolders> {
+    private List<Task> task;
+    protected Context context;
+    public RecyclerViewAdapter(Context context, List<Task> task) {
+        this.task = task;
+        this.context = context;
+    }
+    @Override
+    public RecyclerViewHolders onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerViewHolders viewHolder = null;
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.to_do_list, parent, false);
+        viewHolder = new RecyclerViewHolders(layoutView, task);
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(RecyclerViewHolders holder, int position) {
+        holder.categoryTitle.setText(task.get(position).getTask());
+    }
+    @Override
+    public int getItemCount() {
+        return this.task.size();
+    }
+}
---
+++ app/src/main/java/com/epicodus/todo/Task.java
@@ -0,0 +1,16 @@
+package com.epicodus.todo;
+
+/**
+ * Created by OIG on 10/23/2017.
+ */
+
+public class Task {
+    private String task;
+    public Task() {}
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+}
